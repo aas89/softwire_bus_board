69a4c2b0261af59031231488102b953d6da101cf
xiusi <xiusi.li@softwire.com>
2019-12-11 10:51:58 +0000

    promises for like the 3rd time

--- busBoard.js
+++ busBoard.js
@@ -3,6 +3,7 @@ import ArrivalPrediction from './ArrivalPredictions.js';
 import readline from 'readline-sync';
 import request from 'request';
 import Coordinates from './Coordinates.js';
+import StopInfo from './StopInfo.js';
 
 let postcode = readline.question('please enter the postcode:\t');
 
@@ -23,26 +24,33 @@ function getNearestBusStops(coordinates) {
 
   request(url, (error, response, body) => {
     let nearest_stops = JSON.parse(body).stopPoints.slice(0,2);
-    
-    let stopcodes = nearest_stops.map(x => x.id);
-    let stopnames = nearest_stops.map(x => x.commonName);
 
-    return stopcodes, stopnames;
+    const stopInfo = nearest_stops.map(stop => new StopInfo(stop.commonName, stop.id))
+    
+    // let stopcodes = nearest_stops.map(x => x.id);
+    // let stopnames = nearest_stops.map(x => x.commonName);
+
+    return stopInfo;
   });
 
 }
 
 
-function getArrivalPredictions(stopcodes, stopnames) {
+function getArrivalPredictions(stopInfo) {
 
-  let url = `https://api.tfl.gov.uk/StopPoint/${stopcodes}/Arrivals?app_id=343014cd&app_key=9847cc3d0bbe15906723b4186e3aa518`;
+  for (let j = 0; j<stopInfo.length; j++) {
 
-  request(url, (error, response, body) => {
-    let arrival_predictions = JSON.parse(body);
-    const arrivalPredictionObjects  = arrival_predictions.map(prediction => new ArrivalPrediction(prediction.lineName, prediction.destinationName, prediction.timeToStation))
-    console.log(arrivalPredictionObjects)
-    return arrivalPredictionObjects, stopnames;
-  });
+    let url = `https://api.tfl.gov.uk/StopPoint/${stopInfo[j].stopCode}/Arrivals?app_id=343014cd&app_key=9847cc3d0bbe15906723b4186e3aa518`;
+
+    request(url, (error, response, body) => {
+      let arrival_predictions = JSON.parse(body);
+      const arrivalPredictions  = arrival_predictions.map(prediction => new ArrivalPrediction(prediction.lineName, prediction.destinationName, prediction.timeToStation));
+      
+    });
+
+  }
+
+  return arrivalPredictions;
 
 }
 
@@ -69,38 +77,11 @@ function print(arrivalPredictionObjects, stopnames){
     }
   })
 }
-
+}
 
 
 
 getCoordinates(postcode)
 .then(coordinates => getNearestBusStops(coordinates))
 .then((stopcodes,stopnames) => getArrivalPredictions(stopcodes, stopnames))
-.then((arrivalPredictionObjects, stopnames) => print(arrivalPredictionObjects, stopnames))
-
-
-
-
-
-getCoordinates(postcode, coordinates =>
-  getNearestBusStops(coordinates, (stopcodes, stopnames) => {
-    for (let j = 0; j<stopcodes.length; j++) {
-      getArrivalPredictions(stopcodes[j], arrivalPredictionObjects => {
-
-        console.log("---------------------------------------------------------------")
-        console.log(`Stop name:\t ${stopnames[j]}\n`);
-        for (let i = 0; i < 5; i++) {
-
-          let prediction = arrivalPredictionObjects[i];
-        
-          
-          console.log(`Line name:\t ${prediction.lineName}`);
-          console.log(`Destination:\t ${prediction.destinationName}`);
-          let minutes = parseInt(Number(prediction.timeToStation)/60);
-          console.log(`Time to arrival:\t ${minutes} minutes\n`);
-
-
-      }
-    })
-  }
-  }));
+.then((arrivalPredictionObjects, stopnames) => print(arrivalPredictionObjects, stopnames));
