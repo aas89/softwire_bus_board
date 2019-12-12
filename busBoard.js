0524584ac9c289a15388e2c78e67099711578ae4
xiusi <xiusi.li@softwire.com>
2019-12-12 15:50:02 +0000

    create simple website which allows user to enter postcode and get predictions

--- busBoard.js
+++ busBoard.js
@@ -7,8 +7,10 @@ import request from 'request';
 import express from 'express';
 
 
+
 const app = express();
 const port = 3001;
+app.use(express.static('frontend'));
 
 app.get('/', (req, res) => res.send('Please enter postcode:\tas search?postcode=<your_postcode>'));
 
@@ -32,6 +34,7 @@ function getCoordinates(postcode) {
   return new Promise(function (resolve, reject) {
     // let postcode = readline.question('please enter the postcode:\t');
     let url = `https://api.postcodes.io/postcodes/${postcode}`;
+
     request(url, (error, response, body) => {
 
       if (response.statusCode !== 200) {
@@ -74,35 +77,13 @@ function getArrivalPredictions(stopInfo) {
     request(url, function (error, response, body) {
 
       var arrivalPredictions = JSON.parse(body).slice(0, 5);
-      arrivalPredictions = arrivalPredictions.map(prediction => new ArrivalPrediction(prediction.lineName, prediction.destinationName, prediction.timeToStation));
+      arrivalPredictions = arrivalPredictions.map(prediction => new ArrivalPrediction(prediction.lineName, prediction.destinationName, Math.round((prediction.timeToStation)/60)));
       resolve([arrivalPredictions, stopInfo]);
     })
   })
 }
 
-function printPredictions(arrivalPredictionsArray) {
 
-  for (let j = 0; j < 2; j++) {
-    console.log("---------------------------------------------------------------");
-    console.log(`Stop name:\t ${arrivalPredictionsArray[j][1].stopName}\n`);
-    if (arrivalPredictionsArray[j][0].length === 0) {
-      console.log('Sorry there are no buses due at this bus stop at the moment');
-    } else {
-      for (let i = 0; i < 5; i++) {
-
-        let prediction = arrivalPredictionsArray[j][0][i];
-
-        console.log(`Line name:\t ${prediction.lineName}`);
-        console.log(`Destination:\t ${prediction.destinationName}`);
-        let minutes = parseInt(Number(prediction.timeToStation) / 60);
-        console.log(`Time to arrival:\t ${minutes} minutes\n`);
-      }
-            
-    }
-
-  }
-
-  
 
 
 
---
+++ frontend/departures.js
@@ -0,0 +1,40 @@
+function loadData(postcode){
+    var xhttp = new XMLHttpRequest();
+    
+    xhttp.open('GET', `http://localhost:3001/search?postcode=${postcode}`, true);
+    
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+
+    
+
+    
+    xhttp.onload = function(postcode) {
+
+    
+        const predictions = JSON.parse(this.response);
+
+        document.querySelector("#results").innerHTML = `
+
+     
+        <h3>${predictions[0][1].stopName}</h3>
+        <ul>
+            <li>${predictions[0][0][0].timeToStation} minutes: ${predictions[0][0][0].lineName} to ${predictions[0][0][0].destinationName}</li>
+            <li>${predictions[0][0][1].timeToStation} minutes: ${predictions[0][0][1].lineName} to ${predictions[0][0][1].destinationName}</li>
+            <li>${predictions[0][0][2].timeToStation} minutes: ${predictions[0][0][2].lineName} to ${predictions[0][0][2].destinationName}</li>
+            <li>${predictions[0][0][3].timeToStation} minutes: ${predictions[0][0][3].lineName} to ${predictions[0][0][3].destinationName}</li>
+            <li>${predictions[0][0][4].timeToStation} minutes: ${predictions[0][0][4].lineName} to ${predictions[0][0][4].destinationName}</li>
+        </ul>
+        <h3>${predictions[1][1].stopName}</h3>
+        <ul>
+            <li>${predictions[1][0][0].timeToStation} minutes: ${predictions[1][0][0].lineName} to ${predictions[1][0][0].destinationName}</li>
+            <li>${predictions[1][0][1].timeToStation} minutes: ${predictions[1][0][1].lineName} to ${predictions[1][0][1].destinationName}</li>
+            <li>${predictions[1][0][2].timeToStation} minutes: ${predictions[1][0][2].lineName} to ${predictions[1][0][2].destinationName}</li>
+            <li>${predictions[1][0][3].timeToStation} minutes: ${predictions[1][0][3].lineName} to ${predictions[1][0][3].destinationName}</li>
+            <li>${predictions[1][0][4].timeToStation} minutes: ${predictions[1][0][4].lineName} to ${predictions[1][0][4].destinationName}</li>
+        </ul>`
+    
+            
+    }
+    
+    xhttp.send();
+}
