02466c2bcb5a33f7a24ffa0505aa7bd56983c439
xiusi <xiusi.li@softwire.com>
2019-12-12 16:49:39 +0000

    added error handling for invalid postcode

--- busBoard.js
+++ busBoard.js
@@ -6,8 +6,6 @@ import readline from 'readline-sync';
 import request from 'request';
 import express from 'express';
 
-
-
 const app = express();
 const port = 3001;
 app.use(express.static('frontend'));
@@ -16,13 +14,13 @@ app.get('/', (req, res) => res.send('Please enter postcode:\tas search?postcode=
 
 app.get('/search?', (req, res) => {
   var postcode = req.query.postcode;
-  
-  getCoordinates(postcode)
-  .then(coordinates => getNearestBusStops(coordinates))
-  .then(stopInfoArray => getAllArrivalPredictions(stopInfoArray))
-  .then((arrivalPredictionsArray) => res.send(arrivalPredictionsArray));
 
-  
+  getCoordinates(postcode)
+    .then(coordinates => getNearestBusStops(coordinates))
+    .then(stopInfoArray => getAllArrivalPredictions(stopInfoArray))
+    .then((arrivalPredictionsArray) => res.send(arrivalPredictionsArray))
+    .catch(error => res.send(error));
+
 });
 
 
@@ -32,19 +30,21 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 function getCoordinates(postcode) {
   return new Promise(function (resolve, reject) {
-    // let postcode = readline.question('please enter the postcode:\t');
     let url = `https://api.postcodes.io/postcodes/${postcode}`;
 
     request(url, (error, response, body) => {
 
+
       if (response.statusCode !== 200) {
-        console.log('Invalid Postcode, please try again')
-        getCoordinates();
+        console.log('Invalid Postcode');
+        reject('invalid postcode');
+      } else {
+
+        let location = JSON.parse(body);
+        const coordinateObjects = new Coordinates(location.result.longitude, location.result.latitude);
+        resolve(coordinateObjects)
       }
 
-      let location = JSON.parse(body);
-      const coordinateObjects = new Coordinates(location.result.longitude, location.result.latitude);
-      resolve(coordinateObjects);
     });
   });
 }
@@ -77,22 +77,8 @@ function getArrivalPredictions(stopInfo) {
     request(url, function (error, response, body) {
 
       var arrivalPredictions = JSON.parse(body).slice(0, 5);
-      arrivalPredictions = arrivalPredictions.map(prediction => new ArrivalPrediction(prediction.lineName, prediction.destinationName, Math.round((prediction.timeToStation)/60)));
+      arrivalPredictions = arrivalPredictions.map(prediction => new ArrivalPrediction(prediction.lineName, prediction.destinationName, Math.round((prediction.timeToStation) / 60)));
       resolve([arrivalPredictions, stopInfo]);
     })
   })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
